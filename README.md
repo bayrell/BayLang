b0f9401ab8d44d3107cbabb4e96c022de330bcee
Ildar Bikmamatov <vistoyn@gmail.com>
2017-12-13 09:59:31 +0000

    start

---
+++ README.md
@@ -0,0 +1,3 @@
+# Description
+
+Bayrell Common Language
---
+++ rules.js
@@ -0,0 +1,28 @@
+var bayrell_rtl = require('bayrell_rtl');
+var rtl = bayrell_rtl.rtl;
+
+
+base_path = '';
+if (typeof __dirname != 'undefined') base_path = rtl.clone(__dirname);
+else if (typeof global.__dirname != 'undefined') base_path = rtl.clone(global.__dirname);
+
+
+module.exports = {
+	'type': 1,
+	'base_path': base_path,
+	'files': [
+		'BayrellCode',
+		'BayrellParserBay',
+		'BayrellParserES6',
+		//'BayrellParserPHP',
+		'BayrellTranslator',
+		'BayrellTranslatorES6',
+		'BayrellTranslatorPHP',
+		'BayrellTranslatorBay',
+		'BayrellTranslatorNodeJS',
+		'BayrellInterpreter',
+		'AssetLang',
+		'ModuleInfo',
+	],
+};
+
