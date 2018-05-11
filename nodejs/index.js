81db0926aebc020a035de1e65ff4d950b1a6713c
Ildar Bikmamatov <vistoyn@gmail.com>
2018-05-11 12:18:12 +0000

    v0.2.3

--- nodejs/index.js
+++ nodejs/index.js
@@ -18,7 +18,7 @@
 
 
 module.exports = {
-	VERSION: '0.2.2',
+	VERSION: '0.2.3',
 	'Exceptions': {
 		'EndOfStringExpected': require("./Exceptions/EndOfStringExpected.js"),
 		'HexNumberExpected': require("./Exceptions/HexNumberExpected.js"),
