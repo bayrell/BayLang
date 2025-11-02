156adef538a395390ba9a9434b9624b4d36f9537
Ildar Bikmamatov <ildar@bayrell.org>
2025-11-02 22:50:08 +0500

    translate css style, add component hash

--- nodejs/index.js
+++ nodejs/index.js
@@ -94,6 +94,7 @@ add("BayLang.LangPHP.TranslatorPHPExpression");
 add("BayLang.LangPHP.TranslatorPHPHtml");
 add("BayLang.LangPHP.TranslatorPHPOperator");
 add("BayLang.LangPHP.TranslatorPHPProgram");
+add("BayLang.LangStyle.TranslatorStyle");
 add("BayLang.OpCodes.OpAnnotation");
 add("BayLang.OpCodes.OpAssign");
 add("BayLang.OpCodes.OpAssignStruct");
